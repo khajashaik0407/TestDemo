a14feb7ece03c14336dc13c04fe829906398d4f9
khaja <khaja.shaik0407@gmail.com>
2017-07-06 19:55:14 +0530

    Initial commit for Demo Test

---
+++ TestDemo/src/sampletest/TestDemo.java
@@ -0,0 +1,20 @@
+/** Khaja Shaik 06-Jul-2017
+ * 
+ */
+package sampletest;
+
+/**
+ * @author khaja
+ *
+ */
+public class TestDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo Test");
+	}
+
+}
